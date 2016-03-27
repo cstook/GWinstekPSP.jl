ebc9f527a6810644247adefd42b2cdb419ac71c6
Chris Stook <chris@chrisstook.com>
2016-03-26 23:43:07 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/psp_savepreset.jl
+++ src/psp_savepreset.jl
@@ -6,7 +6,7 @@ export psp_savepreset
 const SAVE_PRESET = [0x45, 0x45, 0x50, 0x0d]
 
 """
-save current configuration to EEPROM
+save current configuration to EEPROM.
 power supply will power up in this state.
 """
 function psp_savepreset(io_psp::IO)
